e219ce5e70a045a1204c24866efd96a60ec9752e
Uthan H <uthan.hercules12@gmail.com>
2021-11-15 11:01:15 -0300

    Agora o C apaga devidamente o texto sem recarregar a página

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+function cClear() {
+  const textArea = document.querySelector('.text');
+
+  textArea.value = '';
+}
+
 function displaynum(n1) {
   Calculator.text1.value = Calculator.text1.value + n1;
 }
